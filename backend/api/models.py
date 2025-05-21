b67860e5beb2c9d1b92948ecd52354ca3fd7506b
Blxee <aminedev57@gmail.com>
2025-05-21 11:37:20 +0100

    Created: Order and Product models (no migration yet)

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Order(models.Model):
+    """Represents an order from a buyer."""
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=256, null=True, blank=True)
+    buyer = models.CharField(max_length=64)
+    product_type = models.ForeignKey(to='Product', on_delete=models.PROTECT)
+    amount = models.PositiveIntegerField(null=True, blank=True)
+    date_started = models.DateField(default=timezone.now, blank=True)
+    date_due = models.DateField(null=True, blank=True)
+    sell_price = models.PositiveIntegerField()
+
+
+class Product(models.Model):
+    """Represents a product type (sphere, spiral, soap bars..)."""
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=256, null=True, blank=True)
+    picture = models.ImageField()
+    work_price = models.PositiveIntegerField()
